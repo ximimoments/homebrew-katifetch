3f7afd3508ab0d8bcb8ca7f98f2441a2dab8d4ca
ximimoments <161499192+ximimoments@users.noreply.github.com>
2026-02-15 16:13:16 -0300

    Update download URL for katifetch to release version

--- Formula/katifetch.rb
+++ Formula/katifetch.rb
@@ -1,7 +1,7 @@
 class Katifetch < Formula
   desc "Minimal and customizable neofetch-like system info tool"
   homepage "https://github.com/ximimoments/katifetch"
-  url "https://github.com/ximimoments/katifetch/archive/refs/tags/v13.1.tar.gz"
+  url "https://github.com/ximimoments/katifetch/releases/download/13.1/katifetch-13.1.tar.gz"
   sha256 "9e0f8d6e20cd7dc874c748732e500121b4c6de0f1ea8438ec7d52d461936410c"
   license "MIT"
 
